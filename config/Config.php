115a864d70e9c3b787c790c258a500e7137cbffa
zrone <xujining2008@126.com>
2021-06-11 10:26:03 +0800

    used

--- config/Config.php
+++ config/Config.php
@@ -14,8 +14,8 @@ class Config
     public const PROJECT = [
         // bf 需要对应 hook url 的 package
         'bf' => [
-            'SECRET'   => '秘钥', // 与WebHooks 签名密钥保持一直
-            'WEB_PATH' => '/www/wwwroot/project', // Web项目工作路径
+            'SECRET'   => '35aad1f91e59e4388a5704b38b8793c988881f02', // 与WebHooks 签名密钥保持一直
+            'WEB_PATH' => '/www/wwwroot/yoshop2.0', // Web项目工作路径
         ],
     ];
 }
